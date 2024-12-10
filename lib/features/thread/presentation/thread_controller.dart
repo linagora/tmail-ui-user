leveretka/tmail-flutter

TF-2646 Show the top bar thread button only when the email list is loaded

lib/features/thread/presentation/thread_controller.dart
@@ -1393,7 +1393,8 @@ class ThreadController extends BaseController with EmailActionController, PopupM
     final listSelectionEmailActions = [
       EmailActionType.markAllAsRead,
       EmailActionType.markAllAsUnread,
-      EmailActionType.moveAll,
+      if (currentMailbox == null || currentMailbox?.isDrafts == false)
+        EmailActionType.moveAll,
       if (currentMailbox?.isTrash == true || currentMailbox?.isSpam == true || currentMailbox?.isDrafts == true)
         EmailActionType.deleteAllPermanently
       else

linagora/tmail-ui-user

lib/features/thread/presentation/thread_controller.dart
@@ -1421,7 +1421,8 @@ class ThreadController extends BaseController with EmailActionController, PopupM
     final listSelectionEmailActions = [
       EmailActionType.markAllAsRead,
       EmailActionType.markAllAsUnread,
-      EmailActionType.moveAll,
+      if (selectedMailbox == null || selectedMailbox?.isDrafts == false)
+        EmailActionType.moveAll,
       if (selectedMailbox?.isTrash == true ||
           selectedMailbox?.isSpam == true ||
           selectedMailbox?.isDrafts == true)