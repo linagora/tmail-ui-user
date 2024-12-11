leveretka/tmail-flutter

TF-2646 Handle move all email searched to Trash

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -3004,6 +3004,27 @@ class MailboxDashBoardController extends ReloadableController {
       failure: failure);
   }
 
+  Future<void> moveAllEmailSearchedToTrash(
+    BuildContext context,
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) async {
+    final trashMailboxId = getMailboxIdByRole(PresentationMailbox.roleTrash);
+
+    if (trashMailboxId == null) return;
+
+    final trashMailboxPath = mapMailboxById[trashMailboxId]?.getDisplayName(context) ?? '';
+
+    consumeState(_moveAllEmailSearchedToFolderInteractor.execute(
+      session,
+      accountId,
+      filterRequest,
+      trashMailboxId,
+      trashMailboxPath
+    ));
+  }
+
   @override
   void onClose() {
     _emailReceiveManager.closeEmailReceiveManagerStream();

linagora/tmail-ui-user

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -3503,6 +3503,27 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
       failure: failure);
   }
 
+  Future<void> moveAllEmailSearchedToTrash(
+    AppLocalizations appLocalizations,
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) async {
+    final trashMailboxId = getMailboxIdByRole(PresentationMailbox.roleTrash);
+
+    if (trashMailboxId == null) return;
+
+    final trashMailboxPath = mapMailboxById[trashMailboxId]?.getDisplayNameByAppLocalizations(appLocalizations) ?? '';
+
+    consumeState(_moveAllEmailSearchedToFolderInteractor.execute(
+      session,
+      accountId,
+      filterRequest,
+      trashMailboxId,
+      trashMailboxPath
+    ));
+  }
+
   @override
   void onClose() {
     if (PlatformInfo.isWeb) {