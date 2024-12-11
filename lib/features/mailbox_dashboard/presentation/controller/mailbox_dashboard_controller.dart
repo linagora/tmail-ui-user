leveretka/tmail-flutter

TF-2646 Handle mark all as starred for search emails

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -139,6 +139,7 @@ import 'package:tmail_ui_user/features/thread/domain/state/get_email_by_id_state
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_as_starred_selection_all_emails_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_as_unread_selection_all_emails_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_read_state.dart';
+import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_starred_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_unread_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_as_multiple_email_read_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_as_star_multiple_email_state.dart';
@@ -151,6 +152,7 @@ import 'package:tmail_ui_user/features/thread/domain/usecases/get_email_by_id_in
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_as_starred_selection_all_emails_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_as_unread_selection_all_emails_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_read_interactor.dart';
+import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_starred_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_unread_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_as_multiple_email_read_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_as_star_multiple_email_interactor.dart';
@@ -207,6 +209,7 @@ class MailboxDashBoardController extends ReloadableController {
   final MarkAllAsStarredSelectionAllEmailsInteractor _markAllAsStarredSelectionAllEmailsInteractor;
   final MarkAllSearchAsReadInteractor _markAllSearchAsReadInteractor;
   final MarkAllSearchAsUnreadInteractor _markAllSearchAsUnreadInteractor;
+  final MarkAllSearchAsStarredInteractor _markAllSearchAsStarredInteractor;
 
   GetAllVacationInteractor? _getAllVacationInteractor;
   UpdateVacationInteractor? _updateVacationInteractor;
@@ -246,6 +249,7 @@ class MailboxDashBoardController extends ReloadableController {
   final markAllAsStarredSelectionAllEmailsViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
   final markAllSearchAsReadViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
   final markAllSearchAsUnreadViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
+  final markAllSearchAsStarredViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
 
   Session? sessionCurrent;
   Map<Role, MailboxId> mapDefaultMailboxIdByRole = {};
@@ -313,6 +317,7 @@ class MailboxDashBoardController extends ReloadableController {
     this._markAllAsStarredSelectionAllEmailsInteractor,
     this._markAllSearchAsReadInteractor,
     this._markAllSearchAsUnreadInteractor,
+    this._markAllSearchAsStarredInteractor,
   );
 
   @override
@@ -436,6 +441,10 @@ class MailboxDashBoardController extends ReloadableController {
       markAllSearchAsUnreadViewState.value = Right(success);
     } else if (success is MarkAllSearchAsUnreadSuccess) {
       _handleMarkAllSearchAsUnreadSuccess(success);
+    } else if (success is MarkAllSearchAsStarredLoading) {
+      markAllSearchAsStarredViewState.value = Right(success);
+    } else if (success is MarkAllSearchAsStarredSuccess) {
+      _handleMarkAllSearchAsStarredSuccess(success);
     }
   }
 
@@ -474,6 +483,8 @@ class MailboxDashBoardController extends ReloadableController {
       _handleMarkAllSearchAsReadFailure(failure);
     } else if (failure is MarkAllSearchAsUnreadFailure) {
       _handleMarkAllSearchAsUnreadFailure(failure);
+    } else if (failure is MarkAllSearchAsStarredFailure) {
+      _handleMarkAllSearchAsStarredFailure(failure);
     }
   }
 
@@ -2888,6 +2899,40 @@ class MailboxDashBoardController extends ReloadableController {
 
   void _handleMarkAllSearchAsUnreadFailure(Failure failure) {
     markAllSearchAsUnreadViewState.value = Right(UIState.idle);
+
+    if (currentContext == null || currentOverlayContext == null) return;
+
+    appToastManager.showFailureMessage(
+      context: currentContext!,
+      overlayContext: currentOverlayContext!,
+      failure: failure);
+  }
+
+  void markAllSearchAsStarred(
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) {
+    consumeState(_markAllSearchAsStarredInteractor.execute(
+      session,
+      accountId,
+      filterRequest
+    ));
+  }
+
+  void _handleMarkAllSearchAsStarredSuccess(Success success) {
+    markAllSearchAsStarredViewState.value = Right(UIState.idle);
+
+    if (currentContext == null || currentOverlayContext == null) return;
+
+    appToastManager.showSuccessMessage(
+      context: currentContext!,
+      overlayContext: currentOverlayContext!,
+      success: success);
+  }
+
+  void _handleMarkAllSearchAsStarredFailure(Failure failure) {
+    markAllSearchAsStarredViewState.value = Right(UIState.idle);
 
     if (currentContext == null || currentOverlayContext == null) return;
 

linagora/tmail-ui-user

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -150,6 +150,7 @@ import 'package:tmail_ui_user/features/thread/domain/state/get_email_by_id_state
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_as_starred_selection_all_emails_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_as_unread_selection_all_emails_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_read_state.dart';
+import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_starred_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_all_search_as_unread_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_as_multiple_email_read_state.dart';
 import 'package:tmail_ui_user/features/thread/domain/state/mark_as_star_multiple_email_state.dart';
@@ -163,6 +164,7 @@ import 'package:tmail_ui_user/features/thread/domain/usecases/get_email_by_id_in
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_as_starred_selection_all_emails_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_as_unread_selection_all_emails_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_read_interactor.dart';
+import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_starred_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_all_search_as_unread_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_as_multiple_email_read_interactor.dart';
 import 'package:tmail_ui_user/features/thread/domain/usecases/mark_as_star_multiple_email_interactor.dart';
@@ -226,6 +228,7 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
   final MarkAllAsStarredSelectionAllEmailsInteractor _markAllAsStarredSelectionAllEmailsInteractor;
   final MarkAllSearchAsReadInteractor _markAllSearchAsReadInteractor;
   final MarkAllSearchAsUnreadInteractor _markAllSearchAsUnreadInteractor;
+  final MarkAllSearchAsStarredInteractor _markAllSearchAsStarredInteractor;
 
   GetAllVacationInteractor? _getAllVacationInteractor;
   UpdateVacationInteractor? _updateVacationInteractor;
@@ -267,6 +270,7 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
   final markAllAsStarredSelectionAllEmailsViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
   final markAllSearchAsReadViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
   final markAllSearchAsUnreadViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
+  final markAllSearchAsStarredViewState = Rx<Either<Failure, Success>>(Right(UIState.idle));
 
   Session? sessionCurrent;
   Map<Role, MailboxId> mapDefaultMailboxIdByRole = {};
@@ -338,6 +342,7 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
     this._markAllAsStarredSelectionAllEmailsInteractor,
     this._markAllSearchAsReadInteractor,
     this._markAllSearchAsUnreadInteractor,
+    this._markAllSearchAsStarredInteractor,
   );
 
   @override
@@ -479,6 +484,10 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
       markAllSearchAsUnreadViewState.value = Right(success);
     } else if (success is MarkAllSearchAsUnreadSuccess) {
       _handleMarkAllSearchAsUnreadSuccess(success);
+    } else if (success is MarkAllSearchAsStarredLoading) {
+      markAllSearchAsStarredViewState.value = Right(success);
+    } else if (success is MarkAllSearchAsStarredSuccess) {
+      _handleMarkAllSearchAsStarredSuccess(success);
     }
   }
 
@@ -522,6 +531,8 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
       _handleMarkAllSearchAsReadFailure(failure);
     } else if (failure is MarkAllSearchAsUnreadFailure) {
       _handleMarkAllSearchAsUnreadFailure(failure);
+    } else if (failure is MarkAllSearchAsStarredFailure) {
+      _handleMarkAllSearchAsStarredFailure(failure);
     }
   }
 
@@ -3387,6 +3398,40 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
 
   void _handleMarkAllSearchAsUnreadFailure(Failure failure) {
     markAllSearchAsUnreadViewState.value = Right(UIState.idle);
+
+    if (currentContext == null || currentOverlayContext == null) return;
+
+    toastManager.showFailureMessage(
+      context: currentContext!,
+      overlayContext: currentOverlayContext!,
+      failure: failure);
+  }
+
+  void markAllSearchAsStarred(
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) {
+    consumeState(_markAllSearchAsStarredInteractor.execute(
+      session,
+      accountId,
+      filterRequest
+    ));
+  }
+
+  void _handleMarkAllSearchAsStarredSuccess(Success success) {
+    markAllSearchAsStarredViewState.value = Right(UIState.idle);
+
+    if (currentContext == null || currentOverlayContext == null) return;
+
+    toastManager.showSuccessMessage(
+      context: currentContext!,
+      overlayContext: currentOverlayContext!,
+      success: success);
+  }
+
+  void _handleMarkAllSearchAsStarredFailure(Failure failure) {
+    markAllSearchAsStarredViewState.value = Right(UIState.idle);
 
     if (currentContext == null || currentOverlayContext == null) return;
 