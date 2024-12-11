leveretka/tmail-flutter

TF-2646 Handle mark all email searched as Spam

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -3025,6 +3025,28 @@ class MailboxDashBoardController extends ReloadableController {
     ));
   }
 
+  Future<void> markAllEmailSearchedAsSpam(
+    BuildContext context,
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) async {
+    final spamMailboxId = getMailboxIdByRole(PresentationMailbox.roleSpam);
+
+    if (spamMailboxId == null) return;
+
+    final spamMailboxPath = mapMailboxById[spamMailboxId]?.getDisplayName(context) ?? '';
+
+    consumeState(_moveAllEmailSearchedToFolderInteractor.execute(
+      session,
+      accountId,
+      filterRequest,
+      spamMailboxId,
+      spamMailboxPath,
+      isDestinationSpamMailbox: true
+    ));
+  }
+
   @override
   void onClose() {
     _emailReceiveManager.closeEmailReceiveManagerStream();

linagora/tmail-ui-user

lib/features/mailbox_dashboard/presentation/controller/mailbox_dashboard_controller.dart
@@ -3524,6 +3524,28 @@ class MailboxDashBoardController extends ReloadableController with UserSettingPo
     ));
   }
 
+  Future<void> markAllEmailSearchedAsSpam(
+    AppLocalizations appLocalizations,
+    Session session,
+    AccountId accountId,
+    SearchEmailFilterRequest filterRequest
+  ) async {
+    final spamMailboxId = getMailboxIdByRole(PresentationMailbox.roleSpam);
+
+    if (spamMailboxId == null) return;
+
+    final spamMailboxPath = mapMailboxById[spamMailboxId]?.getDisplayNameByAppLocalizations(appLocalizations) ?? '';
+
+    consumeState(_moveAllEmailSearchedToFolderInteractor.execute(
+      session,
+      accountId,
+      filterRequest,
+      spamMailboxId,
+      spamMailboxPath,
+      isDestinationSpamMailbox: true
+    ));
+  }
+
   @override
   void onClose() {
     if (PlatformInfo.isWeb) {